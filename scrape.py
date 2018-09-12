89fb97255f299796e1e1e26ae25c79e841355029
klin <ironcarbide@gmail.com>
2018-09-12 15:26:35 -0700

    Initial commit.

--- scrape.py
+++ scrape.py
@@ -2,6 +2,13 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
+import sqlite3
+
+#conn = sqlite3.connect('options.db')
+
+#c = conn.cursor()
+
+#c.execute('')
 
 # variable for url
 quote_page = 'https://finance.yahoo.com/quote/MSFT/options?date=1545350400'
@@ -58,6 +65,13 @@ parentlist.append(onerow)
 
 onerow = []
 row_box = soup.find('tr', attrs={'class': 'data-row9'})
+for col in row_box.find_all('td'):
+    coltext = col.get_text()
+    onerow.append(coltext)
+parentlist.append(onerow)
+
+onerow = []
+row_box = soup.find('tr', attrs={'class': 'data-row10'})
 for col in row_box.find_all('td'):
     coltext = col.get_text()
     onerow.append(coltext)
