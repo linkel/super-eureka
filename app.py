a45a1881f18cc9ba6d83b1499487aea3873a3669
klin <ironcarbide@gmail.com>
2018-10-08 17:58:16 -0700

    Allow selecting table from dropdown to view.

--- app.py
+++ app.py
@@ -19,8 +19,21 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+@app.route("/selection", methods=["GET", "POST"])
+def selection():
+    """After Table Selected"""
+    if request.method == "POST":
+        # get the table name
+        tablepath = request.form.get("symbol")
+        db = get_db()
+        cursor = db.execute("SELECT ContractName, Date, Strike, LastPrice FROM %s" % (tablepath))
+        return render_template('selection.html', items=cursor.fetchall())
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     db = get_db()
-    cursor = db.execute('SELECT ContractName, Date, Strike, LastPrice FROM MSFTCallsJan2019')
-    return render_template('index.html', items=cursor.fetchall())
+    availableTables = db.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
+    listofTables = []
+    for name in availableTables:
+        listofTables.append(name[0])
+    return render_template("index.html", table = listofTables)
